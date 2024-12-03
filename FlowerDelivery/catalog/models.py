c06e30ce9e5218aaa55e6b5866a6e16016969cb1
SanaGo108 <oksana135@mail.ru>
2024-12-03 16:52:23 +0300

    add

--- FlowerDelivery/catalog/models.py
+++ FlowerDelivery/catalog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(default="Описание отсутствует")  # Значение по умолчанию
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/')  # Загружаются в папку media/products/
+
+    def __str__(self):
+        return self.name
